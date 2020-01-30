90bf21a9b8784ad9f60e29090e26874b7036052f
TienDat2308 <tiendat2308@gmail.com>
2020-01-30 13:47:16 +0700

    test lodash

--- lodash.js
+++ lodash.js
@@ -2,5 +2,7 @@ const lodash = require('lodash');
 
 console.log(lodash.range(1514764800, 1514772001, 7200));
 let array = ['foo', 'bar'];
+const arr = [];
 console.log(`create table abc ${lodash.join(array, '')}`);
 console.log(lodash.isArray([]));
+console.log(lodash.isEmpty(arr));

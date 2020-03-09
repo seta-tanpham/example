0ab09c4be6c082a09c5213503c985350707cdafa
seta-tanpham <datdt@ip-192-168-43-127.us-west-2.compute.internal>
2020-03-09 16:48:01 +0700

    test

--- lodash.js
+++ lodash.js
@@ -4,3 +4,4 @@ console.log(lodash.range(1514764800, 1514772001, 7200));
 let array = ['foo', 'bar'];
 console.log(`create table abc ${lodash.join(array, '')}`);
 console.log(lodash.isArray([]));
+console.log(lodash.isEmpty([]));
